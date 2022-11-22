c1c6f865aa134bd1314655dae393580260626928
kadync3 <106933582+kadync3@users.noreply.github.com>
2022-11-22 13:04:40 -0500

    completed all bees with es6 classes

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,16 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
   // TODO..
+  constructor (){
+    super ();
+    this.age = 40;
+    this.color ='grey'
+    this.canFly = false
+    this.job = 'gamble'
+  }
+  forage (){
+    return 'I am too old, let me play cards instead'
+  }
+  gamble (treasure){
+ this.treasureChest.push(treasure)
+  }
 };
